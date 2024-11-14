9724302348eee3f7a90e8c2203253abae9e1131a
Jurić Josip <0135278172@FPZ.HR>
2024-11-14 09:42:46 +0100

    dodavanje koda

--- git.py
+++ git.py
@@ -1,1 +1,14 @@
-print("Dobrodošli na Računalstvo u prometnom inženjerstvu")
+def provjera_broja (broj):
+    if 10 <= broj <= 100:
+        return f"Broj {broj} je unutar raspona."
+    else:
+        return f"Broj {broj} je izvan raspona."
+    
+if __name__ == "__main___":
+    try:
+        prvi_broj = int(input("Unesite prvi broj: "))
+        print(provjera broja(prvi broj))
+        drugi_broj = int(input("Unesite drugi broj: "))
+        print(provjera_broja    (drugi_broj))
+    except ValueError:
+        print("Unesena vrijednost nije broj.")
